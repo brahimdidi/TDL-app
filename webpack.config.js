8043a04e32925e6c81b8f9030b401fdad8f3ed9f
brahim <brahimgb1team@gmail.com>
2022-02-19 14:17:54 +0000

    fix eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-
   entry: './src/index.js',
-
-  devServer: {
-    static: './dist',
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-
+  mode: 'development',
   module: {
     rules: [
       {
@@ -29,4 +13,14 @@ module.exports = {
       },
     ],
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
 };
